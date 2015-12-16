9e6209b66f2b27b8e2cb19b3104f65acc232e53c
David Sorokin <david.sorokin@gmail.com>
2015-12-16 08:47:29 +0300

    Updated OutputMessageQueue.

--- Simulation/Aivika/Distributed/Optimistic/Internal/OutputMessageQueue.hs
+++ Simulation/Aivika/Distributed/Optimistic/Internal/OutputMessageQueue.hs
@@ -31,10 +31,8 @@ import Simulation.Aivika.Distributed.Optimistic.DIO
 
 -- | Specifies the output message queue.
 data OutputMessageQueue =
-  OutputMessageQueue { outputMessages :: Vector Message,
+  OutputMessageQueue { outputMessages :: Vector Message
                        -- ^ The output messages.
-                       outputMessageIndex :: IORef Int
-                       -- ^ An index of the next actual item.
                      }
 
 -- | Lift the 'IO' computation in an unsafe manner.
@@ -49,14 +47,41 @@ liftIOUnsafe = liftComp . DIO . liftIO
 createOutputMessageQueue :: Simulation DIO OutputMessageQueue
 createOutputMessageQueue =
   do ms <- liftIOUnsafe0 newVector
-     r  <- liftIOUnsafe0 $ newIORef 0
-     return OutputMessageQueue { outputMessages = ms,
-                                 outputMessageIndex = r }
+     return OutputMessageQueue { outputMessages = ms }
 
 -- | Send the message.
 sendMessage :: OutputMessageQueue -> Message -> Event DIO ()
-sendMessage = undefined
+sendMessage q m =
+  do when (messageSendTime m > messageReceiveTime m) $
+       error "The Send time cannot be greater than the Receive message time: sendMessage"
+     n <- liftIOUnsafe $ vectorCount (outputMessages q)
+     when (n > 0) $
+       do m' <- liftIOUnsafe $ readVector (outputMessages q) (n - 1)
+          when (messageSendTime m' > messageSendTime m) $
+            error "A new output message comes from the past: sendMessage."
+     liftIOUnsafe $ appendVector (outputMessages q) m
+     doSendMessage q m
 
 -- | Rollback the messages till the specified time including that one.
 rollbackMessages :: OutputMessageQueue -> Double -> Event DIO ()
-rollbackMessages = undefined
+rollbackMessages q t =
+  do ms <- extractMessagesToRollback q t
+     forM_ ms (doSendMessage q . antiMessage)
+                 
+-- | Return the messages to roolback by the specified time.
+extractMessagesToRollback :: OutputMessageQueue -> Double -> Event DIO [Message]
+extractMessagesToRollback q t =
+  let loop i acc
+        | i < 0     = return acc
+        | otherwise =
+          do m <- liftIOUnsafe $ readVector (outputMessages q) i
+             if messageSendTime m < t
+               then return acc
+               else do liftIOUnsafe $ vectorDeleteAt (outputMessages q) i
+                       loop (i - 1) (m : acc)
+  in do n <- liftIOUnsafe $ vectorCount (outputMessages q)
+        loop (n - 1) []
+                 
+-- | Do send a message on low level.
+doSendMessage :: OutputMessageQueue -> Message -> Event DIO ()
+doSendMessage = undefined
