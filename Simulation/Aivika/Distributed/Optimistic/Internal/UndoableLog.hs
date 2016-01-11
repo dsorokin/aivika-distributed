0946395b568b2bba1bc28f0a6e716c459a09baee
David Sorokin <david.sorokin@gmail.com>
2016-01-11 18:10:18 +0300

    Updated module UndoableLog.

--- Simulation/Aivika/Distributed/Optimistic/Internal/UndoableLog.hs
+++ Simulation/Aivika/Distributed/Optimistic/Internal/UndoableLog.hs
@@ -13,12 +13,13 @@ module Simulation.Aivika.Distributed.Optimistic.Internal.UndoableLog
        (UndoableLog,
         newUndoableLog,
         writeLog,
-        rollbackLog) where
+        rollbackLog,
+        reduceLog) where
 
 import Control.Monad
 import Control.Monad.Trans
 
-import Simulation.Aivika.Vector
+import qualified Simulation.Aivika.DoubleLinkedList as DLL
 import Simulation.Aivika.Trans.Comp
 import Simulation.Aivika.Trans.Simulation
 import Simulation.Aivika.Trans.Dynamics
@@ -29,7 +30,7 @@ import Simulation.Aivika.Distributed.Optimistic.Internal.IO
 
 -- | Specified an undoable log with ability to rollback the operations.
 data UndoableLog =
-  UndoableLog { logItems :: Vector UndoableItem
+  UndoableLog { logItems :: DLL.DoubleLinkedList UndoableItem
                 -- ^ The items that can be undone.
               }
 
@@ -43,7 +44,7 @@ data UndoableItem =
 -- | Create an undoable log.
 newUndoableLog :: DIO UndoableLog
 newUndoableLog =
-  do xs <- liftIOUnsafe newVector
+  do xs <- liftIOUnsafe DLL.newList
      return UndoableLog { logItems = xs }
 
 -- | Write a new undoable operation.
@@ -51,17 +52,26 @@ writeLog :: UndoableLog -> DIO () -> Event DIO ()
 writeLog log h =
   do t <- liftDynamics time
      let x = UndoableItem { itemTime = t, itemUndo = h }
-     liftIOUnsafe $ appendVector (logItems log) x
+     liftIOUnsafe $ DLL.listAddLast (logItems log) x
 
 -- | Rollback the log till the specified time including that one.
 rollbackLog :: UndoableLog -> Double -> Event DIO ()
 rollbackLog log t =
-  do n <- liftIOUnsafe $ vectorCount (logItems log)
-     when (n > 0) $
-       do x <- liftIOUnsafe $ readVector (logItems log) (n - 1)
+  do f <- liftIOUnsafe $ DLL.listNull (logItems log)
+     unless f $
+       do x <- liftIOUnsafe $ DLL.listLast (logItems log)
           when (t <= itemTime x) $
-            do liftIOUnsafe $ vectorDeleteAt (logItems log) (n - 1)
+            do liftIOUnsafe $ DLL.listRemoveLast (logItems log)
                liftComp (itemUndo x)
                rollbackLog log t
 
+-- | Reduce the log removing all items older than the specified time.
+reduceLog :: UndoableLog -> Double -> Event DIO ()
+reduceLog log t =
+  do f <- liftIOUnsafe $ DLL.listNull (logItems log)
+     unless f $
+       do x <- liftIOUnsafe $ DLL.listFirst (logItems log)
+          when (itemTime x < t) $
+            do liftIOUnsafe $ DLL.listRemoveFirst (logItems log)
+               reduceLog log t
 
