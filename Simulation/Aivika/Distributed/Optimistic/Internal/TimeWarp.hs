6fb6dc3d8059c4379749838bbbf8662a25b72df3
David Sorokin <david.sorokin@gmail.com>
2016-02-20 17:11:12 +0300

    Updated module TimeWarp.

--- Simulation/Aivika/Distributed/Optimistic/Internal/TimeWarp.hs
+++ Simulation/Aivika/Distributed/Optimistic/Internal/TimeWarp.hs
@@ -13,38 +13,11 @@ module Simulation.Aivika.Distributed.Optimistic.Internal.TimeWarp
        (TimeWarp(..),
         invokeTimeWarp) where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Trans
-
 import Simulation.Aivika.Trans
 import Simulation.Aivika.Trans.Internal.Types
 
 -- | The time warp computation.
 newtype TimeWarp m a = TimeWarp (Point m -> m a)
-
-instance Monad m => Monad (TimeWarp m) where
-
-  {-# INLINE return #-}
-  return = TimeWarp . const . return
-
-  {-# INLINE (>>=) #-}
-  (TimeWarp m) >>= k = TimeWarp $ \p ->
-    m p >>= \a ->
-    let TimeWarp m' = k a in m' p
-
-instance Applicative m => Applicative (TimeWarp m) where
-
-  {-# INLINE pure #-}
-  pure = TimeWarp . const . pure
-
-  {-# INLINE (<*>) #-}
-  (TimeWarp x) <*> (TimeWarp y) = TimeWarp $ \p -> x p <*> y p
-
-instance Functor m => Functor (TimeWarp m) where
-
-  {-# INLINE fmap #-}
-  fmap f (TimeWarp m) = TimeWarp $ fmap f . m 
 
 -- | Invoke the 'TimeWarp' computation.
 invokeTimeWarp :: Point m -> TimeWarp m a -> m a
